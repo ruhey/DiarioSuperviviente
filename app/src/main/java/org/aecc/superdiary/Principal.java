5751adc88c1bdd88716748cb14424a09057bd1a4
ruhey <ruhey@hotmail.com>
2015-05-10 21:47:37 +0200

    prueba de commit

--- app/src/main/java/org/aecc/superdiary/Principal.java
+++ app/src/main/java/org/aecc/superdiary/Principal.java
@@ -28,6 +28,7 @@ public class Principal extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+        int id2 = item.getItemId();
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
