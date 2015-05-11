aafd9fc643f14bef22d32990a79c3a61aea51464
ruhey <ruhey@hotmail.com>
2015-05-11 21:46:33 +0200

    Imagen principal y pruebas de colores

---
+++ app/src/main/java/org/aecc/superdiary/BienvenidaActivity.java
@@ -0,0 +1,57 @@
+package org.aecc.superdiary;
+
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+public class BienvenidaActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bienvenida);
+
+
+        Button botonBienvenida = (Button)findViewById(R.id.botonBienvenida);
+        botonBienvenida.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(BienvenidaActivity.this, Principal.class);
+                startActivity(intent);
+            }
+        });
+
+        ImageView imagenLogo = (ImageView)findViewById(R.id.imagenEntrada);
+        imagenLogo.setImageResource(R.drawable.aeccLogo);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_bienvenida, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
