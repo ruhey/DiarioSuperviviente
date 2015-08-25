021583bf84dd9c99f15486ff000dcdb664caad74
ruhey <ruhey@hotmail.com>
2015-08-25 14:55:03 +0200

    layouts

--- app/src/main/java/org/aecc/superdiary/presentation/view/activity/MeditacionTab.java
+++ app/src/main/java/org/aecc/superdiary/presentation/view/activity/MeditacionTab.java
@@ -2,7 +2,6 @@ package org.aecc.superdiary.presentation.view.activity;
 
         import android.os.Bundle;
         import android.support.annotation.Nullable;
-        import android.support.v4.app.Fragment;
         import android.view.LayoutInflater;
         import android.view.View;
         import android.view.ViewGroup;
